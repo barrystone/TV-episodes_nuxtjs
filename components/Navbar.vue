0f91f11252ec56b94f859ae2647b49a00b398b01
barrystone <barrystone3183@gmail.com>
2022-08-28 23:54:55 +0800

    [feat] new Navbar for show page

---
+++ components/Navbar.vue
@@ -0,0 +1,105 @@
+<script lang="ts">
+export default {
+  name: 'Navbar',
+  props: ['showName'],
+  setup(props) {
+    const name = ref('');
+    watchEffect(() => {
+      name.value = props.showName;
+    });
+    return { name };
+  }
+};
+</script>
+
+<template>
+  <nav
+    class="p-5 bg-gray-50 rounded border-gray-200 dark:bg-gray-800 dark:border-gray-700"
+  >
+    <div class="container flex flex-wrap justify-between items-center mx-auto">
+      <a href="#" class="flex items-center">
+        <nuxt-link
+          to="/"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer ml-5"
+          >Back to Home
+
+          <svg
+            aria-hidden="true"
+            class="ml-1 -mr-1 w-4 h-4"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </nuxt-link>
+        <span
+          class="self-center text-xl font-semibold whitespace-nowrap dark:text-white ml-10"
+        >
+          {{ name }}</span
+        >
+      </a>
+      <button
+        data-collapse-toggle="navbar-solid-bg"
+        type="button"
+        class="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500"
+        aria-controls="navbar-solid-bg"
+        aria-expanded="false"
+      >
+        <span class="sr-only">Open main menu</span>
+        <svg
+          class="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+        <ul
+          class="flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700"
+        >
+          <li>
+            <a
+              href="#"
+              class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent"
+              aria-current="page"
+              >Home</a
+            >
+          </li>
+          <li>
+            <a
+              href="#episodes"
+              class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Episodes</a
+            >
+          </li>
+          <li>
+            <a
+              href="#casts"
+              class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Casts</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Contact</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
--- pages/show/[id]/index.vue
+++ pages/show/[id]/index.vue
@@ -22,7 +22,8 @@ onMounted(async () => {
 
 <template>
   <div>
-    <header class="flex items-center bg-white py-4 shadow-lg">
+    <Navbar :showName="showInfo.name" />
+    <!-- <header class="flex items-center bg-white py-4 shadow-lg">
       <nuxt-link
         to="/"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer h-10 ml-5"
@@ -43,9 +44,13 @@ onMounted(async () => {
         </svg>
       </nuxt-link>
       <h3 class="ml-10">{{ showInfo.name }}</h3>
-    </header>
+    </header> -->
     <main>
-      <ShowEpisodes v-bind:showInfo="showInfo" v-bind:episodes="episodes" />
+      <ShowEpisodes
+        v-if="$route.hash === '#episodes'"
+        :showInfo="showInfo"
+        :episodes="episodes"
+      />
     </main>
   </div>
 </template>
