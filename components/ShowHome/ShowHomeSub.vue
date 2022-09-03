996b1bbf5e452c54150911e609e4b94b501eca78
barrystone <barrystone3183@gmail.com>
2022-09-03 21:53:28 +0800

    [feat] showHomeSub story display

--- components/ShowHome/ShowHomeSub.vue
+++ components/ShowHome/ShowHomeSub.vue
@@ -137,88 +137,17 @@ export default {
         </dl>
       </div>
       <div
-        class="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800"
+        class="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800 flex justify-center items-center"
         :class="tabId === 'story-tab' ? '' : 'hidden'"
         id="about"
         role="tabpanel"
         aria-labelledby="about-tab"
       >
-        <h2
-          class="mb-5 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-white"
-        >
-          We invest in the world’s potential
-        </h2>
-        <!-- List -->
-        <ul role="list" class="space-y-4 text-gray-500 dark:text-gray-400">
-          <li class="flex space-x-2">
-            <!-- Icon -->
-            <svg
-              class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span class="font-light leading-tight"
-              >Dynamic reports and dashboards</span
-            >
-          </li>
-          <li class="flex space-x-2">
-            <!-- Icon -->
-            <svg
-              class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span class="font-light leading-tight">Templates for everyone</span>
-          </li>
-          <li class="flex space-x-2">
-            <!-- Icon -->
-            <svg
-              class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span class="font-light leading-tight">Development workflow</span>
-          </li>
-          <li class="flex space-x-2">
-            <!-- Icon -->
-            <svg
-              class="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span class="font-light leading-tight"
-              >Limitless business automation</span
-            >
-          </li>
-        </ul>
+        <div
+          class="text-white w-[80%] py-5 leading-9 text-lg tracking-wide [&>p>b]:text-yellow-100 [&>p>b]:px-2"
+          v-html="showInfo.summary"
+        />
+        <!-- {{ showInfo.summary?.slice(3, -4).startsWith("<b>")?showInfo.summary?.slice(3, -4).split('>')[2]:showInfo.summary?.slice(3, -4) }} -->
       </div>
       <div
         class="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800"
